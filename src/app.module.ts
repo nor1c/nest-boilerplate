e31379a15aca3f6af4e3c01927fc4670c1c68f24
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2024-01-30 13:20:17 +0700

    move env file to project directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './core/auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
-import { AuthModule } from './core/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: 'environments/.env',
     }),
     DatabaseModule,
     UserModule,
